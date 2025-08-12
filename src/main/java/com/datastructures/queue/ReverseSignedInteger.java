b29be13699f04d7788d9d0158950df67902d9a00
saranshbansal <kratos.saransh@gmail.com>
2025-08-12 16:52:45 +0530

    add stack and queue related programs and basic operations

---
+++ src/main/java/com/datastructures/queue/ReverseSignedInteger.java
@@ -0,0 +1,23 @@
+package com.datastructures.queue;
+
+public class ReverseSignedInteger {
+    public static void main(String[] args) {
+        int[] nums = {123, -123, 0, 1534236469, -2147483412, 1563847412};
+        for (int num : nums) {
+            System.out.println(reverse(num));
+        }
+    }
+
+    private static int reverse(int num) {
+        int rev = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            if (rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+            rev = rev * 10 + digit;
+            num /= 10;
+        }
+        return rev;
+    }
+}
---
+++ src/main/java/com/datastructures/queue/ReverseString.java
@@ -0,0 +1,22 @@
+package com.datastructures.queue;
+
+import java.util.Stack;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        char[] arr = {'h', 'e', 'l', 'l', 'o'};
+
+        Stack<Character> stack = new Stack<>();
+        for (Character c : arr) {
+            stack.push(c);
+        }
+
+        int i = 0;
+        while (!stack.isEmpty()) {
+            arr[i] = stack.pop();
+            i++;
+        }
+
+        System.out.println(arr);
+    }
+}
